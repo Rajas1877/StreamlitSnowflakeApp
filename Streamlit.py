000c48e87c260d8ca92a9ae3a9c6883a70c549d0
Rajas1877 <rajas2@systechusa.com>
2025-05-12 21:47:20 +0530

    Add files via upload

---
+++ Streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#title - 
+st.title("Welcome to the Streamlit World")
+
+# select box
+st.selectbox("Please choose your Income tax regime",["Old Tax Regime","New Tax Regime"])
+
+#ballon
+st.balloons()
